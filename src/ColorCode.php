0468735ba0a54554ea12b349f23ac94b310cab64
V. Artemenkov <vchslvartemenkov@gmail.com>
2020-08-06 05:25:41 +0300

    add caching code

--- src/ColorCode.php
+++ src/ColorCode.php
@@ -14,41 +14,47 @@ namespace Aslava12\ColorCLI;
    */
   class ColorCode {
 
+    private $cacheCode = [];
+
     function __get($name) {
       $isLight = 1;
       $color = Null;
       $name = "_".strtolower($name);
       $isBG = 0;
 
-      if (strpos($name,"dark")) {
-        $isLight=0;
-      }
+      if (!isset($this->cacheCode[$name])) {
 
-      if (strpos($name,"black")) {
-        $color = 0;
-      } else if (strpos($name,"red")) {
-        $color = 1;
-      } else if (strpos($name,"green")) {
-        $color = 2;
-      } else if (strpos($name,"yellow")) {
-        $color = 3;
-      } else if (strpos($name,"blue")) {
-        $color = 4;
-      } else if (strpos($name,"magenta")) {
-        $color = 5;
-      } else if (strpos($name,"cyan")) {
-        $color = 6;
-      } else if (strpos($name,"white")) {
-        $color = 7;
-      } else {
-        return 0;
-      }
+        if (strpos($name,"dark")) {
+          $isLight=0;
+        }
 
-      if (strpos($name,"bg") or strpos($name,"background")) {
-        $isBG = 1;
-      }
+        if (strpos($name,"black")) {
+          $color = 0;
+        } else if (strpos($name,"red")) {
+          $color = 1;
+        } else if (strpos($name,"green")) {
+          $color = 2;
+        } else if (strpos($name,"yellow")) {
+          $color = 3;
+        } else if (strpos($name,"blue")) {
+          $color = 4;
+        } else if (strpos($name,"magenta")) {
+          $color = 5;
+        } else if (strpos($name,"cyan")) {
+          $color = 6;
+        } else if (strpos($name,"white")) {
+          $color = 7;
+        } else {
+          return 0;
+        }
 
-      return 30 + $isLight * 60 + $isBG * 10 + $color;
+        if (strpos($name,"bg") or strpos($name,"background")) {
+          $isBG = 1;
+        }
+
+        $this->cacheCode[$name] = 30 + $isLight * 60 + $isBG * 10 + $color;
+      }
+      return $this->cacheCode[$name];
 
     }
 
--- src/StyleCode.php
+++ src/StyleCode.php
@@ -18,35 +18,40 @@ namespace Aslava12\ColorCLI;
  */
 class StyleCode {
 
+  private $cacheCode = [];
+
   function __get($name) {
     $name = "_" . strtolower($name);
     $code = 0;
 
-    if (strpos($name,"overline") or strpos($name,"oline")) {
-      $code = 52;
-    } else if (strpos($name,"doubleunderline") or strpos($name,"duline")) {
-      $code = 21;
-    } else if (strpos($name,"bold")) {
-      $code = 1;
-    } else if (strpos($name,"halfbold") or strpos($name,"hbold")) {
-      $code = 2;
-    } else if (strpos($name,"italic")) {
-      $code = 3;
-    } else if (strpos($name,"underline") or strpos($name,"uline")) {
-      $code = 4;
-    } else if (strpos($name,"flash")) {
-      $code = 5;
-    } else if (strpos($name,"revers")) {
-      $code = 7;
-    } else if (strpos($name,"onecolorise") or strpos($name,"ocolor")) {
-      $code = 8;
-    } else if (strpos($name,"cross")) {
-      $code = 9;
-    } else {
-      $code = 0;
-    }
+    if (!isset($this->cacheCode[$name])) {
 
-    return $code;
+      if (strpos($name,"overline") or strpos($name,"oline")) {
+        $code = 52;
+      } else if (strpos($name,"doubleunderline") or strpos($name,"duline")) {
+        $code = 21;
+      } else if (strpos($name,"bold")) {
+        $code = 1;
+      } else if (strpos($name,"halfbold") or strpos($name,"hbold")) {
+        $code = 2;
+      } else if (strpos($name,"italic")) {
+        $code = 3;
+      } else if (strpos($name,"underline") or strpos($name,"uline")) {
+        $code = 4;
+      } else if (strpos($name,"flash")) {
+        $code = 5;
+      } else if (strpos($name,"revers")) {
+        $code = 7;
+      } else if (strpos($name,"onecolorise") or strpos($name,"ocolor")) {
+        $code = 8;
+      } else if (strpos($name,"cross")) {
+        $code = 9;
+      } else {
+        $code = 0;
+      }
+      $this->cacheCode[$name] = $code;
+    }
+    return $this->cacheCode[$name];
 
   }
 
